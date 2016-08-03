d750b3c8022a105574e14aaf96e6c05933de46cc
Niilas Lindblad <LindbNB0@interoute.com>
2016-08-03 16:05:41 +0300

    get-meme14

--- bot.js
+++ bot.js
@@ -74,7 +74,7 @@ const actions = {
       context.name = name; // store it in context
 	  //context.sex = sex;
 	}
-	 if(sex=="male") {
+	 if(sex==="male") {
 	 context.url = 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name='+name+'&sex=m';
 	 }else{
 	  context.url = 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name='+name+'&sex=f';
