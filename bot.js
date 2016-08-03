962ad98d7188785648cb0dd325decafd8be76c0e
Niilas Lindblad <LindbNB0@interoute.com>
2016-08-03 17:01:48 +0300

    get-meme16

--- bot.js
+++ bot.js
@@ -81,7 +81,8 @@ const actions = {
 	  }
 	  //context.url = 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name='+name+'&sex=m';
       cb(context);
-	}
+	},
+	
     error(sessionId, context, error) {
     console.log(error.message);
   },
