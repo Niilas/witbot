1a6ca5a7d5523729576a832fb2bef546900a2e12
Niilas Lindblad <LindbNB0@interoute.com>
2016-08-03 13:56:39 +0300

    get-meme12

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ const actions = {
       cb();
     }
   },
-  const name = 'nimi';
+  const name;
   merge(sessionId, context, entities, message, cb) {
     // Retrieve the location entity and store it into a context field
     name = firstEntityValue(entities, 'contact');
