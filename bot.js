a44be9c0697e1193f6c380ad019cbdeb7af9f45e
Niilas Lindblad <LindbNB0@interoute.com>
2016-08-03 14:03:32 +0300

    get-meme14

--- bot.js
+++ bot.js
@@ -57,9 +57,9 @@ const actions = {
   //const name;
   merge(sessionId, context, entities, message, cb) {
     // Retrieve the location entity and store it into a context field
-    name = firstEntityValue(entities, 'contact');
-    if (name) {
-      context.name = name; // store it in context
+    const global.name = firstEntityValue(entities, 'contact');
+    if (global.name) {
+      context.name = global.name; // store it in context
     }
 
     cb(context);
@@ -73,7 +73,7 @@ const actions = {
   ['get-meme'](sessionId, context, cb) {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loca)
-    context.url = 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name='+name+'&sex=m';
+    context.url = 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name='+global.name+'&sex=m';
     cb(context);
   },
 };
