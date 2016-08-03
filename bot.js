a6fa0d0f4f36fe607599ac26f4c1a32698d781a3
Niilas Lindblad <LindbNB0@interoute.com>
2016-08-03 13:33:24 +0300

    get-meme7

--- bot.js
+++ bot.js
@@ -69,12 +69,12 @@ const actions = {
   },
 
   // get-meme action here
-  ['get-meme'](sessionId, context, cb) {
+  //['get-meme'](sessionId, context, cb) {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loca)
-    context.url = 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name='+context.name+'&sex=m';
-    cb(context);
-  },
+    //context.url = 'http://belikebill.azurewebsites.net/billgen-API.php?default=1&name='+name+'&sex=m';
+   // cb(context);
+  //},
 };
 
 
